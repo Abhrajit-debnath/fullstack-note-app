55e3a378b253f6c57898aa8c76b06593f214e493
Abhrajit Debnath <137435505+Abhrajit-debnath@users.noreply.github.com>
2025-09-03 18:20:48 +0530

    some minor path fixes for deployment

--- backend/src/config/firebase.ts
+++ backend/src/config/firebase.ts
@@ -1,8 +1,36 @@
-import admin from "firebase-admin";
-import serviceAccount from "./serviceAccount.json";
+// import admin from "firebase-admin";
+// import serviceAccount from "./serviceAccount.json";
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+// });
+// const db = admin.firestore();
+// export { admin, db };
+
+
+import admin from "firebase-admin";
+import fs from "fs";
+import path from "path";
+
+let serviceAccount: admin.ServiceAccount;
+
+// Check if local file exists
+const localPath = path.resolve(__dirname, "serviceAccount.json");
+if (fs.existsSync(localPath)) {
+  serviceAccount = require(localPath);
+} else {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT as string);
+}
+
+// Initialize Firebase Admin
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
+
+// Export both admin and Firestore DB
 const db = admin.firestore();
-export { admin, db };
+
+export { admin, db }; // named exports
+
